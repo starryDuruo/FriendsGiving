c2137ec295b2b5bd20efe36dc02953a66c6f44a5
Sige Wang <wangfiz@bc.edu>
2026-03-09 19:26:39 -0400

    guest list and detail

---
+++ FriendsGiving/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  FriendsGiving
+//
+//  Created by Wang Sige on 3/9/26.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ FriendsGiving/Guest.swift
@@ -0,0 +1,8 @@
+//
+//  Guest.swift
+//  FriendsGiving
+//
+//  Created by Wang Sige on 3/9/26.
+//
+
+import Foundation
